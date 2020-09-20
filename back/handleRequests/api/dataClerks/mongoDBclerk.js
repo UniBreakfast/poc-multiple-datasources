240353a72e588ea47ca1cebdcd9baf4c50b4d647
unibreakfast <unibreakfast1@gmail.com>
2020-09-20 23:58:44 +0300

    mongo connected

--- back/handleRequests/api/dataClerks/mongoDBclerk.js
+++ back/handleRequests/api/dataClerks/mongoDBclerk.js
@@ -1,3 +1,18 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const { MongoClient } = require('mongodb')
+
+const dbName = process.env.MDS_DB_NAME
+
+const uri = `mongodb+srv://MDS_admin:${process.env.MDS_DB_PASS
+  }@cluster0.x0yir.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+const mdsDB = new MongoClient(uri, options).connect()
+                    .then(client => client.db(dbName))
+
 export default async function operate(action, subject, data) {
-  return {some: "result"}
+  const db = await mdsDB.catch(console.log)
+  if (`${action} ${subject}` == 'read notes') {
+    return db.collection(subject).find().toArray()
+  }
 }
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+// MDS stands for "multiple datasources" - the name/purpose of this project
 Object.assign(process.env, {
   MDS_ADMIN_KEY: 'MDS_permission_key',
   MDS_DB_IN_USE: 'mongoDB',
-
+  MDS_DB_NAME: 'MDS_db',
+  MDS_DB_PASS: 'MDS_correct_password',
 })
